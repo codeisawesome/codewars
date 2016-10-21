cca74c7b1556eeb8d65d788481d54bbe4910918f
Rui Dong <rui.dong.jh@gmail.com>
2016-10-20 23:28:37 -0400

    learned .index, it returns the index of the element if the element is in the array

--- finding_which_num_is_diff_odd_even.rb
+++ finding_which_num_is_diff_odd_even.rb
@@ -10,4 +10,12 @@ def iq_test(numbers)
       odd.push(x)
     end
   end
+
+  if even.length > odd.length
+    num_array.index(odd[0]) + 1
+  else
+    num_array.index(even[0]) + 1
+  end
 end
+
+iq_test("1 2 3 7 5 9")
