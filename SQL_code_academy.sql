c48a0e1dd7ca5db414d8f051417ad575b292d2db
Rui Dong <rui.dong.jh@gmail.com>
2016-11-08 19:42:43 -0500

    doing some subquery sum and average

--- SQL_code_academy.sql
+++ SQL_code_academy.sql
@@ -4,3 +4,18 @@ WHERE origin in(
   SELECT code
   FROM airports
   WHERE faa_region = "ASO");
+
+-- Using a subquery, find the average total distance flown by day of week and month.
+SELECT a.dep_month,
+       a.dep_day_of_week,
+       AVG(a.flight_distance) AS average_distance
+  FROM (
+        SELECT dep_month,
+              dep_day_of_week,
+               dep_date,
+               sum(distance) AS flight_distance
+          FROM flights
+         GROUP BY 1,2,3
+       ) a
+ GROUP BY 1,2
+ ORDER BY 1,2;
